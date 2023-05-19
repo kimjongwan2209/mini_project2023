573635f89c858560f6e31afadd26b7f5335ccfbc
kimjongwan2209 <aegis0918@naver.com>
2023-05-20 03:28:38 +0900

    mongoose updateone modify

--- backend/router/login.js
+++ backend/router/login.js
@@ -2,16 +2,18 @@ import express from "express";
 import middleware from "../middleware/auth.js";
 import jwt from "jsonwebtoken";
 import { users } from "../model/userSchema.js";
-import * as bcrypt from "bcrypt";
 import { authies } from "../model/authSchema.js";
+import * as bcrypt from "bcrypt";
 
 const router = express.Router();
 
-let tokenObject = {};
+const tokenObject = {};
+
 router.post("/login", async (req, res) => {
   const { name, pwd } = req.body;
 
   try {
+    //변수모음
     const hash = bcrypt.hashSync(pwd, 12);
     const match_password = bcrypt.compareSync(pwd, hash);
     const create_login_date = new Date();
@@ -19,6 +21,7 @@ router.post("/login", async (req, res) => {
       create_login_date.getDate() + 1
     );
 
+    //토큰검증
     if (match_password) {
       const accessToken = jwt.sign({ name: users.name }, "secret", {
         expiresIn: "1200s",
@@ -31,9 +34,8 @@ router.post("/login", async (req, res) => {
         if (result.length == 0) {
           return res.status(400).json({ message: "로그인에 실패하였습니다." });
         }
+
         authies.find({ name }).then((result) => {
-          console.log(result);
-          console.log(name);
           if (result.length === 0) {
             authies
               .create({
@@ -43,15 +45,28 @@ router.post("/login", async (req, res) => {
                 expires_date: create_expires_date,
               })
               .then((result) => {
-                return res.status(200).json({ message: "토큰발급완료" });
+                return res.status(200).json({
+                  message: `${name}님의 토큰이 정상적으로 발급되었습니다`,
+                });
               });
           }
+
           if (result.length !== 0) {
-            authies.updateOne({ name, refreshToken }).then((result) => {
-              return res.status(200).json({ message: "로그인 되었습니다." });
-            });
+            authies
+              .updateOne({
+                name,
+                refreshToken: refreshToken,
+                login_date: create_login_date,
+                expires_date: create_expires_date,
+              })
+              .then((result) => {
+                return res.status(200).json({
+                  message: `${name}님 환영합니다. 접속해주셔서 감사합니다.`,
+                });
+              });
           }
         });
+
         // 토큰 생성, 쿠키에 기록
         tokenObject[refreshToken] = name;
         res.cookie("accessToken", accessToken);
@@ -60,7 +75,7 @@ router.post("/login", async (req, res) => {
       });
     }
   } catch (err) {
-    res.status(400).send({ message: err + " : login failed" });
+    res.status(400).send({ message: err + " : 로그인 접속불가" });
   }
 });
 
