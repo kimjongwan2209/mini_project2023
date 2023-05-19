e27ffb38e5ea92996c3423f90c3db4d61df472d7
kimjongwan2209 <aegis0918@naver.com>
2023-05-19 21:16:58 +0900

    third commit

--- backend/router/login.js
+++ backend/router/login.js
@@ -7,21 +7,21 @@ const router = express.Router();
 
 router.post("/login", async (req, res) => {
   const { name, pwd } = req.body;
-
-  const match_password = 
-
   try {
-    const accessToken = jwt.sign({ user_id: user.user_id }, "secret", {
-      expiresIn: "1200s",
-    });
-    const refreshToken = jwt.sign({}, "secret", {
-      expiresIn: "3d",
-    });
+    const match_password = bcrypt.compareSync(pwd, hash);
+    if (match_password) {
+      const accessToken = jwt.sign({ user_id: user.user_id }, "secret", {
+        expiresIn: "1200s",
+      });
+      const refreshToken = jwt.sign({}, "secret", {
+        expiresIn: "3d",
+      });
       // 토큰 생성, 쿠키에 기록
-      tokenObject[refreshToken] = user_id;
+      tokenObject[refreshToken] = name;
       res.cookie("accessToken", accessToken);
       res.cookie("refreshToken", refreshToken);
-      res.cookie("user_id", user_id);
+      res.cookie("user_id", name);
+    }
   } catch (err) {
     res.status(400).send({ message: err + " : login failed" });
   }
