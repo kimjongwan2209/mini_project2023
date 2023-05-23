1910145213a4dc8c85a74efbcb8a88b914cde60d
kimjongwan2209 <aegis0918@naver.com>
2023-05-23 21:23:28 +0900

    final commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mainRouter from "./router/index.js";
+import connect from "./model/index.js";
+import cors from "cors";
+
+const app = express();
+connect();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", [mainRouter]);
+
+// Backend API 서버 오픈
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`);
+});
--- backend/webcrawler/coffee_crawler.js
+++ backend/webcrawler/coffee_crawler.js
@@ -3,23 +3,34 @@ import mongoose from "mongoose";
 import { starbucks } from "../model/starbucksSchema.js";
 
 export async function startCrawling() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://www.starbucks.co.kr/menu/drink_list.do");
-  await page.waitForTimeout(2400);
+  try {
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto("https://www.starbucks.co.kr/menu/drink_list.do");
+    await page.waitForTimeout(5000);
+  } catch (err) {
+    const err_mesaage = "연결 중 오류가 발생하였습니다.";
+    throw Error("브라우저를 실행 할 수 없습니다." + err);
+    console.log(err_mesaage, err);
+  }
 
   const cnt = await page.$$eval(
-    `#container > div.content > div.product_result_wrap.product_result_wrap01 > div > dl > dd:nth-child(2) > div.product_list > dl > dd `,
+    `#container > div.content > div.product_result_wrap.product_result_wrap01 > div > dl > dd:nth-child(2) > div.product_list > dl > dd`,
     (data) => data.length
   );
+
   console.log(cnt);
 
-  for (let i = 1; i <= cnt; i++) {
+  for (let i = 1; i <= cnt * 2; i++) {
     const second_cnt = await page.$$eval(
       `#container > div.content > div.product_result_wrap.product_result_wrap01 > div > dl > dd:nth-child(2) > div.product_list > dl > dd:nth-child(${i}) > ul > li`,
       (data) => data.length
     );
+    console.log(second_cnt);
 
     for (let j = 1; j <= second_cnt; j++) {
       const image = await page.$eval(
